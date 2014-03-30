46afd2491d95380085d4f432d939116afaba5432
rustolio <david.djdim@gmail.com>
2014-03-29 21:19:48 -0600

    como estaba en vaio
    Archivos como estaban en vaio ahora en la mac

--- fablaboax/fablaboax/settings.py
+++ fablaboax/fablaboax/settings.py
@@ -7,17 +7,23 @@ https://docs.djangoproject.com/en/1.6/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
-
+#encoding:utf-8
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+RUTA_PROYECTO = os.path.dirname(os.path.realpath(__file__))
 
+ADMINS = (
+    ('David Diaz', 'david.djdim@gmail.com'),
+    )
+
+SITE_ID = 1
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u0ag)_aj+h+-khx9obwys2tvwq+2x6ic21@x#jx_$k=z6@6(!*'
+SECRET_KEY = 'xtb)x$sm6=acwm5pe*lg=i_ym94-)vr#v^tx83+e@lh_$8k$9i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -26,6 +32,9 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+TEMPLATE_DIRS = (
+    os.path.join(RUTA_PROYECTO,'plantillas'),
+    )
 
 # Application definition
 
@@ -36,6 +45,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.admindocs',
+    'inventario'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -58,7 +70,7 @@ WSGI_APPLICATION = 'fablaboax.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'fablaboax.db'),
     }
 }
 
@@ -78,5 +90,40 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
+MEDIA_URL = 'http://127.0.0.1:8000/media/'
+MEDIA_ROOT =  os.path.join(RUTA_PROYECTO,'carga')
+
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(RUTA_PROYECTO,'static'),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+#ROOT_URLCONF = 'fablab.urls'
+
+# Python dotted path to the WSGI application used by Django's runserver.
+#WSGI_APPLICATION = 'fablab.wsgi.application'
+
+TEMPLATE_DIRS = (
+    os.path.join(RUTA_PROYECTO,'plantillas'),
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+
